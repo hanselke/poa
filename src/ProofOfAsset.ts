bd8503082078e10f4a5a6a6c1502e5e544944acf
hansel ke <hanselke@MacBook-Pro-4.local>
2022-12-05 12:04:05 +0800

    unverified num example

--- src/ProofOfAsset.ts
+++ src/ProofOfAsset.ts
@@ -20,7 +20,7 @@ class VerifierData extends Struct({
 class ProofOfAsset extends SmartContract {
   @state(Field) AssetVerifierUrlHash = State<Field>();
   @state(Field) MinBalance = State<Field>();
-
+  @state(Field) AssetProofsMerkleTreeRoot = State<Field>();
   @method initState(_AssetVerifierUrlHash: Field, _MinBalance: Field) {
     // need to ensure this can only be called once
     const _AssetVerifierUrlHash_ = this.AssetVerifierUrlHash.get();
@@ -30,10 +30,15 @@ class ProofOfAsset extends SmartContract {
     this.AssetVerifierUrlHash.set(_AssetVerifierUrlHash);
     this.MinBalance.set(_MinBalance);
   }
-  @method verifyAsset(verifierData: VerifierData) {
+  @method verifyAsset(verifierCreds: VerifierData) {
     // maybe we need to go for an access token/tmp url sort of way? so that fetch happens inside here
     // like fetch(www.dbs.com.sg/?token=)
+    // fetch from your browser native state that i know is really from www.dbs.com.sg (UNLESS YOU HACKED YOUR BROWSER)
     const assetVerifierUrl_ = this.AssetVerifierUrlHash.get();
+    const unverifiedNumber = 1;
+    console.log('unverifiedNumber', unverifiedNumber);
+    const verifiedFieldArray: Field = Field(unverifiedNumber);
+    verifiedFieldArray.assertEquals(unverifiedNumber);
     this.AssetVerifierUrlHash.assertEquals(assetVerifierUrl_);
     Circuit.log(
       'ProofOfAsset:verifyAsset:assetVerifierUrl_',
@@ -45,7 +50,8 @@ class ProofOfAsset extends SmartContract {
     const MinBalance_ = this.MinBalance.get();
     Circuit.log('ProofOfAsset:verifyAsset:MinBalance_', MinBalance_);
     this.MinBalance.assertEquals(MinBalance_);
-    const magicBalance = verifierData.relevantTransactions[0].credit; // this obj doesnt work
+
+    const magicBalance = verifierCreds.relevantTransactions[0].credit; // this obv doesnt work
     MinBalance_.assertLte(magicBalance);
   } // this to get this execution success proof
 }
